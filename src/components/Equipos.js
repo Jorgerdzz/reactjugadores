4b4a450045c0e891a364eeecc80755c8013bc656
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-22 19:28:41 +0200

    Implementación imagen de carga de datos de la API

--- src/components/Equipos.js
+++ src/components/Equipos.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react'
 import Global from '../Global'
 import axios from 'axios'
 import { NavLink } from 'react-router-dom'
+import imagen from '../assets/images/loading.gif';
 
 export default class Equipos extends Component {
 
     url = Global.apiEjemplos
 
     state = {
-        equipos: []
+        equipos: [],
+        status: false
     }
 
     loadEquipos = () => {
         let request = "api/Equipos";
         axios.get(this.url + request).then(response => {
             this.setState({
-                equipos: response.data
+                equipos: response.data,
+                status: true
             })
         })
     }
@@ -23,36 +26,39 @@ export default class Equipos extends Component {
         this.loadEquipos();
     }
 
-    deleteEquipos = () => {
-
-    }
 
   render() {
     return (
       <div>
-        <h1 className='text-center m-3'>Equipos</h1>
-        <div className='d-flex'>
-            {
-                this.state.equipos.map((equipo, index)=>{
-                    return(
-                        <div className='row'>
-                            <div className='col-md-4 m-2 d-flex justify-content-center w-100'>
-                                <div key={index} className='card m-2 d-flex align-items-center' style={{width: "20rm"}}>
-                                    <img src={equipo.imagen} className="card-img-top" style={{width: "100px", height: "125px"}} alt={equipo.nombre}></img>
-                                    <div className="card-body flex-column justify-content-center">
-                                        <h5 className="card-title">{equipo.nombre}</h5>
-                                        <p className="card-text">Champions: {equipo.champions}</p>
-                                        <a href={equipo.web}>Sitio web</a><br></br>
-                                        <NavLink to={"/jugadores/" + equipo.idEquipo} className="btn btn-primary d-grid mt-2">Ver jugadores</NavLink>
-                                        <NavLink to={"/createjugador/" + equipo.idEquipo} className="btn btn-warning d-grid mt-2">Añadir jugador</NavLink>
+        {
+            !this.state.status ?
+            <img src={imagen}></img>:
+            <div>
+                <h1 className='text-center m-3'>Equipos</h1>
+                <div className='d-flex'>
+                    {
+                        this.state.equipos.map((equipo, index)=>{
+                            return(
+                                <div className='row'>
+                                    <div className='col-md-4 m-2 d-flex justify-content-center w-100'>
+                                        <div key={index} className='card m-2 d-flex align-items-center' style={{width: "20rm"}}>
+                                            <img src={equipo.imagen} className="card-img-top" style={{width: "100px", height: "125px"}} alt={equipo.nombre}></img>
+                                            <div className="card-body flex-column justify-content-center">
+                                                <h5 className="card-title">{equipo.nombre}</h5>
+                                                <p className="card-text">Champions: {equipo.champions}</p>
+                                                <a href={equipo.web}>Sitio web</a><br></br>
+                                                <NavLink to={"/jugadores/" + equipo.idEquipo} className="btn btn-primary d-grid mt-2">Ver jugadores</NavLink>
+                                                <NavLink to={"/createjugador/" + equipo.idEquipo} className="btn btn-warning d-grid mt-2">Añadir jugador</NavLink>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        </div>  
-                    )
-                })
-            }
-        </div>
+                                </div>  
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        }
       </div>
     )
   }
